95c9e24b06346744b583f61c321ba3e052dd538d
erpModerntechnology <102586159+erpModerntechnology@users.noreply.github.com>
2022-12-10 16:10:55 +0200

    Add files via upload

---
+++ employee_shift/models/employee_archive_resaon.py
@@ -0,0 +1,25 @@
+from odoo import models, fields, api
+class HrDepartureWizardemployee(models.TransientModel):
+    _inherit = 'hr.departure.wizard'
+
+    departure_reason = fields.Selection([
+        ('fired', 'Fired'),
+        ('resigned', 'Resigned'),
+        ('retired', 'Retired'),
+        ('termination', 'Termination'),
+        ('leave_work', 'Leave Work'),
+        ('open_vacation', 'Open Vacation')
+
+    ], string="Departure Reason", default="fired")
+
+class EmployeeArchiveInherit(models.Model):
+    _inherit = 'hr.employee'
+    departure_reason = fields.Selection([
+        ('fired', 'Fired'),
+        ('resigned', 'Resigned'),
+        ('retired', 'Retired'),
+        ('termination', 'Termination'),
+        ('leave_work', 'Leave Work'),
+        ('open_vacation', 'Open Vacation')
+
+    ], string="Departure Reason", default="fired")
